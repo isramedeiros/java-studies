6633412451b89457089e2a925b10614f74727522
Israel Medeiros <israelmfreitas12@gmail.com>
2026-01-10 16:06:55 -0300

    OOP tests

--- src/entities/Triangle.java
+++ src/entities/Triangle.java
@@ -5,6 +5,8 @@ public class Triangle {
     public double b;
     public double c;
 
+    // double é o tipo de dado que o metodo retorna
+    // se o metodo não retorna nada, usa-se void
     public double area() {
         double p = (a + b + c) / 2.0;
         return Math.sqrt(p * (p - a) * (p - b) * (p - c));
